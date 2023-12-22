d0aaff10c83e54665aebb0d76f132049962275dc
Vishal <vishalsharma05052002@gmail.com>
2023-12-22 21:46:16 +0530

    Final Push

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,17 +1,40 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const {Admin,Course} =require('../db/index')
 const router = Router();
 
 // Admin Routes
 <<<<<<< HEAD
+<<<<<<< HEAD
 app.post('/signup', (req, res) => {
     // Implement admin signup logic
+=======
+router.post('/signup', async(req, res) => {
+    try {
+        const { username, password } = req.body;
+        const newAdmin = new Admin({ username, password });
+        await newAdmin.save();
+        res.status(200).json({ message: 'Admin created successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+>>>>>>> parent of d07fd92 (week 3 push)
 });
 
-app.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post('/courses', adminMiddleware, async(req, res) => {
+    try {
+        const { title, description, price, imageLink } = req.body;
+        const newCourse = new Course({ title, description, price, imageLink });
+        await newCourse.save();
+        res.status(200).json({ message: 'Course saved successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
+<<<<<<< HEAD
 app.get('/courses', adminMiddleware, (req, res) => {
 =======
 router.post('/signup', (req, res) => {
@@ -25,6 +48,16 @@ router.post('/courses', adminMiddleware, (req, res) => {
 router.get('/courses', adminMiddleware, (req, res) => {
 >>>>>>> 86bdc525b6a13a9b10f6a3babb28fcc538497275
     // Implement fetching all courses logic
+=======
+router.get('/courses', adminMiddleware, async(req, res) => {
+    try {
+        const courses = await Course.find();
+        res.status(200).json({ courses });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+>>>>>>> parent of d07fd92 (week 3 push)
 });
 
 module.exports = router;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,24 +1,87 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const {Course,User,PurchasedCourse}=require('../db/index')
 
 // User Routes
+<<<<<<< HEAD
 <<<<<<< HEAD
 app.post('/signup', (req, res) => {
     // Implement user signup logic
 });
+=======
+router.post('/signup', async(req, res) => {
+    const {username, password} = req.body;
+    const newUser= new User({username,password});
+    await newUser.save();
+    res.status(200).json({ message: 'User created successfully' });
 
-app.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+>>>>>>> parent of d07fd92 (week 3 push)
+
 });
 
-app.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.get('/courses', async(req, res) => {
+    try {
+        const courses = await Course.find();
+        res.status(200).json({ courses });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+        
+    }
+
 });
 
-app.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
+    try {
+        const username = req.headers.username;
+        const courseId = req.params.courseId;
+
+        const user = await User.findOne({ username, password: req.headers.password });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        // Use findOne to find the course by courseId
+        const course = await Course.findOne({ _id: courseId });
+
+        if (!course) {
+            return res.status(404).json({ error: 'Course not found' });
+        }
+
+
+        const existingPurchase = await PurchasedCourse.findOne({ userId: user._id, courseId });
+
+        if (existingPurchase) {
+            return res.status(400).json({ error: 'Course already purchased by the user' });
+        }
+        const newPurchasedCourse = new PurchasedCourse({
+            userId: user._id,
+            courseId: course._id,
+        });
+
+        await newPurchasedCourse.save();
+        res.status(200).json({ message: 'Course purchased successfully' });
+    } catch (error) {
+        console.error('Error in purchase route:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+
+router.get('/purchasedCourses', userMiddleware, async(req, res) => {
     // Implement fetching purchased courses logic
+    try {
+        const user = req.user;
+        const purchasedCourses = await PurchasedCourse.find({ userId: user._id }).populate('courseId');
+        res.status(200).json({ purchasedCourses });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
+<<<<<<< HEAD
 =======
 router.post('/signup', (req, res) => {
     // Implement user signup logic
@@ -38,3 +101,7 @@ router.get('/purchasedCourses', userMiddleware, (req, res) => {
 
 module.exports = router
 >>>>>>> 86bdc525b6a13a9b10f6a3babb28fcc538497275
+=======
+
+module.exports=router;
+>>>>>>> parent of d07fd92 (week 3 push)
